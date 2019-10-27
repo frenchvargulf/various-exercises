4ad86ddb033fa4da57cec31b6476b070501a404d
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-10-27 20:47:53 +0100

    Add promise resolve to 3 ex

--- axios-promises-functional/app.js
+++ axios-promises-functional/app.js
@@ -1,66 +1,105 @@
-const URLS = ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"];
+const URLs = ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"];
 const BASE_URL = 'http://ip-api.com';
 
-const companiesList = document.querySelector('.promises-companies-list');
-const asyncList = document.querySelector('.async-companies-list');
+const urls = URLs.map( (url) => {
+  return `${BASE_URL}/json/${url}`;
+} )
 
-
-const createSpan = (element, el) => {
-  const span = document.createElement("span");
-  span.innerText = ` ${element} `;
-  el.append(span)
+const getAllData = (URLS) => {
+  let axiosPromises = URLS.map(fetchData);
+  return Promise.all(axiosPromises);
 }
 
-const createLi = (data, list) => {
-  const el = document.createElement("li");
+// async function getAllData(URLS){
+//   let axiosPromises = URLS.map(fetchData);
+//   return await Promise.all(axiosPromises);
+// }
 
-  createSpan(data.as, el)
-  createSpan(data.query, el)
-  createSpan(data.country, el)
-  createSpan(data.isp, el)
-
-  list.append(el)
+const fetchData = URL => {
+  return axios
+    .get(URL)
+    .then( response => {
+      return response.data
+    })
+    .catch( (error) => {
+      return { success: false };
+    });
 }
 
-const PromiseAxios = (url) => {
-  axios.get(`${BASE_URL}/json/${url}`)
-  .then(function (response) {
-    // handle success
-    const data = response.data;
-    createLi(data, companiesList)
-    return data;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-  });
-}
+getAllData(urls).then( resp => console.log(resp) ).catch( e => console.log(e) )
 
-const getAsyncResponse = async (url) => {
-  try {
-    const res = await axios.get(`${BASE_URL}/json/${url}`);
-    const ARRAY = res.data;
+// const companiesList = document.querySelector('.promises-companies-list');
+// const asyncList = document.querySelector('.async-companies-list');
 
-    createLi(ARRAY, asyncList)
-    return ARRAY;
-  } catch (e) {
-    console.error(e);
-  }
-};
+// var promise1 = Promise.resolve(3);
+// var promise2 = 42;
+// var promise3 = new Promise(function(resolve, reject) {
+//   setTimeout(resolve, 100, 'foo');
+// });
 
-const getAsyncData = (urls) => {
-  urls.map( (url) => {
-    return getAsyncResponse(url)
-  } )
-}
+// Promise.all([promise1, promise2, promise3]).then(function(values) {
+//   console.log(values);
+// });
 
-const getData = (urls) => {
-  urls.map( (url) => {
-    return PromiseAxios(url, asyncList)
-  } )
-}
+// expected output: Array [3, 42, "foo"]
 
-getData(URLS)
-getAsyncData(URLS)
+
+// const createSpan = (element, el) => {
+//   const span = document.createElement("span");
+//   span.innerText = ` ${element} `;
+//   el.append(span)
+// }
+
+// const createLi = (data, list) => {
+//   const el = document.createElement("li");
+
+//   createSpan(data.as, el)
+//   createSpan(data.query, el)
+//   createSpan(data.country, el)
+//   createSpan(data.isp, el)
+
+//   list.append(el)
+// }
+
+// const PromiseAxios = (url) => {
+//   axios.get(`${BASE_URL}/json/${url}`)
+//   .then(function (response) {
+//     // handle success
+//     const data = response.data;
+//     createLi(data, companiesList)
+//     return data;
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//   });
+// }
+
+// const getAsyncResponse = async (url) => {
+//   try {
+//     const res = await axios.get(`${BASE_URL}/json/${url}`);
+//     const ARRAY = res.data;
+
+//     createLi(ARRAY, asyncList)
+//     return ARRAY;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
+
+// const getAsyncData = (urls) => {
+//   urls.map( (url) => {
+//     return getAsyncResponse(url)
+//   } )
+// }
+
+// const getData = (urls) => {
+//   urls.map( (url) => {
+//     return PromiseAxios(url, asyncList)
+//   } )
+// }
+
+// getData(URLS)
+// getAsyncData(URLS)
